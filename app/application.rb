a68ba3d0d84bc8a994df401a11cd79c7de38ed0d
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-04 18:31:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    if 
+    if
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
-      
+
     end
   
