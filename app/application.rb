c7d6af802f269b87767e5dcc2a01d2fecfabace1
Chris Kerbo <chris.kerbo@outlook.com>
2019-11-04 18:33:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
-
+    clock = Time.now.to_i
+    binding.pry
     if Time.now.to_i < 12
       resp.write "Good Morning!"
     else
